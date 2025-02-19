b98a0b99bd70a38fe361e47b677e7f327f3a1d0e
OrgilDev <155336618+orgildev@users.noreply.github.com>
2025-02-18 19:45:31 -0800

    FINALLY

---
+++ UBC Student Service Centre_files/grades.js
@@ -0,0 +1,57 @@
+function switchTab(tabId) {
+    // Remove active classes from all tabs
+    $('.ui-state-default').removeClass('ui-tabs-active ui-state-active');
+    $('a[href="#' + tabId + '"]').parent().addClass('ui-tabs-active ui-state-active');
+    
+    // Hide all panels and show the selected one
+    $('.ui-tabs-panel').hide();
+    $('#' + tabId).show();
+    
+    // Update current tab and recalculate
+    currentTab = tabId.replace('tabs-', '');
+    calculate();
+}
+
+function calculate() {
+    if (doTop) {
+        refreshTop();
+    } else {
+        recalculate();
+    }
+}
+
+function recalculate() {
+    doTop = false;
+    var numSelected = $("#tabs-"+currentTab+" tr.listHighlight").size();
+    var totalCredits = 0;
+    var totalGrade = 0;
+    
+    $("#tabs-"+currentTab+" td.listRow[grade]").each(
+        function(i) {
+            if ($(this).parent().hasClass("listHighlight")) {
+                var gradeText = $(this).text();
+                var grade = parseInt(gradeText);
+                var credits = parseFloat($(this).attr("credits"));
+                if (!(isNaN(grade) || isNaN(credits))) {
+                    totalCredits += credits;
+                    totalGrade += (credits * grade);
+                }
+            }
+        }
+    );
+    
+    var avgGrade = Math.round(totalGrade / totalCredits * 10) / 10;
+    $("#calculator_result").text(isNaN(avgGrade) ? "0%" : avgGrade + "%");
+}
+
+$(document).ready(function() {
+    // Initialize click handlers for tabs
+    $('.ui-tabs-anchor').click(function(e) {
+        e.preventDefault();
+        var tabId = $(this).attr('href').substring(1); // Remove the # from href
+        switchTab(tabId);
+    });
+    
+    // Initialize with 2024W tab
+    switchTab('tabs-2024W');
+});
